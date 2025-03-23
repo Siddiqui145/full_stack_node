57caea200ee0b9367730ca1206782696c432a391
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-23 15:49:19 +0530

    printing data about jokes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
+  //get through axios
+  useEffect(() => {
+    axios.get('/api/jokes').then((response) => {
+      setJokes(response.data) //just get data and paste it this way
+    }).catch((error) => {
+      console.log(error);
+    })
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Full Stack Application</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Jokes : {jokes.length}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {
+          jokes.map((joke, index) => (
+            <div key= {joke.id}>
+              <h3> {joke.title} </h3>
+              <p> {joke.content} </p>
+            </div>
+          ))
+        }
     </>
   )
 }
