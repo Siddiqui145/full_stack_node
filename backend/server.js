291bd2009fd42698d322f12d9e905464b6cb6722
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-23 15:17:29 +0530

    added jokes data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
     res.send('Server is Ready');
 });
 
+app.get('/quotes', (req, res) => {
+    const quotes = [
+        { "id": 1, "title": "Inspiration", "content": "Believe you can and you're halfway there." },
+        { "id": 2, "title": "Motivation", "content": "The secret of getting ahead is getting started." },
+        { "id": 3, "title": "Wisdom", "content": "Do what you can, with what you have, where you are." },
+        { "id": 4, "title": "Perseverance", "content": "Fall seven times, stand up eight." },
+        { "id": 5, "title": "Success", "content": "Success is not final, failure is not fatal: It is the courage to continue that counts." }
+      ];
+    res.send(quotes);
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
